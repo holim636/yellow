4c1e4b455f1e7b4cfc68de53631dda1b04a0a5d2
holim636 <holim636@naver.com>
2022-02-14 17:46:46 +0900

    Update data_init.py
    수정 완료

--- yellow/data_init.py
+++ yellow/data_init.py
@@ -1,7 +1,6 @@
 import csv
 
 
-
 dataPath = 'res/data/서울특별시_' #데이터 경로
 gulist = ['강남구','강북구','관악구','동작마포서대문영등포','송파광진성동','양천구_구로구_금천구','1']
 filetype = '.csv' #파일 형식
@@ -9,6 +8,7 @@ school_num=[2, 1, 0, 1, 1, 1, 1] #학교명이 들어가는 열
 date=[4, 4, 2, 2, 2, 2, 2]
 data= dict()
 data_list = []
+data_date = []
 for i , gu in enumerate(gulist):# enumrate로 몇번째 행인지와 구 각각 불러오기
 
     file_name = dataPath + gu + filetype # 위에서 지정한 경로와 구 명, 파일형식합쳐서 파일위치 만들기
@@ -18,8 +18,7 @@ for i , gu in enumerate(gulist):# enumrate로 몇번째 행인지와 구 각각 
         for line in data:#csv객체 각 줄 불러서
             if(line[school_num[i]].find("초등학교")!=-1):
                 #print(line[school_num[i]]+','+line[date[i]])
-                #data[line[school_num[i]]] = line[date[i]]
                 data_list.append(line[school_num[i]])
-                
-#print(data)
-print(data_list)
+                data_date.append(line[date[i]])
+result = dict(zip(data_list, data_date))
+print(result)
